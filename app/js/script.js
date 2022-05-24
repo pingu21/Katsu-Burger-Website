9b0cbfff3fd6a1c482df3283dc37158cea818b5c
pingu21 <80418269+pingu21@users.noreply.github.com>
2022-05-23 19:11:44 -0700

    in=progress coding hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,14 @@
-function scrollToTop(){
-  window.scrollTo({top: 0, behavior: 'smooth'});
+const hamburger = document.querySelector(".hamburger");
+const navbarMenu = document.querySelector(".navbar-menu");
+
+
+window.onload=function(){
+  function scrollToTop(){
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  
+  }
+  
+  hamburger.addEventListener("click", () => {
+    navbarMenu.classList.toggle("active");
+  })
 }
