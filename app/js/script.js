0386b191c87662e3e314818fe39e2b3c2863fe98
pingu21 <80418269+pingu21@users.noreply.github.com>
2022-05-23 22:10:00 -0700

    code fixed, favicon added,

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,4 @@
-  function scrollToMenu(){
-    var element = document.getElementById("menu-anchor");
-    element.scrollIntoView({behavior: "smooth"});
-  }
-
-
-  function scrollToTop(){
-    window.scrollTo({top: 0, behavior: "smooth"});
-  
-  }
+function scrollToMenu(){
+  var element = document.getElementById("menu-anchor");
+  element.scrollIntoView({behavior: "smooth"});
+}
