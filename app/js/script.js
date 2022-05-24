c52d525fe812a78092abb8f79a532bcfeae7adbc
pingu21 <80418269+pingu21@users.noreply.github.com>
2022-05-23 20:02:45 -0700

    changed logo

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,5 @@
-const hamburger = document.querySelector(".hamburger");
-const navbarMenu = document.querySelector(".navbar-menu");
 
-
-window.onload=function(){
   function scrollToTop(){
     window.scrollTo({top: 0, behavior: 'smooth'});
   
   }
-  
-  hamburger.addEventListener("click", () => {
-    navbarMenu.classList.toggle("active");
-  })
-}
