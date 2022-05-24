7b112e72660d7bac31b4ce6e3642c4ce8ad42e4a
pingu21 <80418269+pingu21@users.noreply.github.com>
2022-05-23 22:18:01 -0700

    had to re-add scroll to top

--- app/js/script.js
+++ app/js/script.js
@@ -2,3 +2,8 @@ function scrollToMenu(){
   var element = document.getElementById("menu-anchor");
   element.scrollIntoView({behavior: "smooth"});
 }
+
+function scrollToTop(){
+  window.scrollTo({top: 0, behavior: "smooth"});
+
+}
